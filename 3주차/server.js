2bd2656d97b16720998e7e29c6bedd1d91875268
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-03 22:24:47 +0900

    feat: refactor server and router to handle routing
    - move routing logic to router.js for better separation of concerns
    - update server.js to require and use router for handling requests
    - pass pathname from server to router for logging purposes
    - modify index.js to integrate server and router modules

--- 3주차/server.js
+++ 3주차/server.js
@@ -1,7 +1,11 @@
 let http = require('http');
+let url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
+        let pathname = url.parse(request.url).pathname;
+        route(pathname);
+
         response.writeHead(200, {'Content-Type' : 'text/html'});
         response.write('Hello Node.js');
         response.end();
