d17eeb4a49ce60a4bacb49f174b062538fdba832
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-05 10:31:56 +0900

    chore: add comments to database query in main function

--- 3주차/requestHandler.js
+++ 3주차/requestHandler.js
@@ -1,8 +1,10 @@
+// 데이터베이스 연결 모듈 불러오기
 const mariadb = require('D:/DevCourse-6/3주차/database/connect/mariadb.js')
 
 function main(response) {
     console.log('Response : main');
 
+    // 'product' 테이블에서 모든 데이터 조회 -> 조회된 데이터 출력
     mariadb.query("SELECT * FROM product", function(err, rows) {
         console.log(rows);
     });
