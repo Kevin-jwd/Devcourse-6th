0851737748da2603e5cdefbca8067e177c9317c7
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-12 22:30:59 +0900

    feat: /test 엔드포인트 추가

--- 4주차/node_base/express-demo.js
+++ 4주차/node_base/express-demo.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+
+// GET + "/"
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+// API : GET + "http://localhost:1234/test"
+app.get('/test', function(req, res) {
+    res.send('TEST SUCCESS');
 })
 
 app.listen(1234)
