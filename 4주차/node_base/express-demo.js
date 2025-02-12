bd37f0cfd2327c654e39b0c9abbf1d5f7bd1678d
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-13 06:18:27 +0900

    feat: /products/1 엔드 포인트 추가
    - res.send()를 res.json()으로 대체

--- 4주차/node_base/express-demo.js
+++ 4주차/node_base/express-demo.js
@@ -1,32 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(1234)
+app.listen(1234);
 
-// GET + "/"
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// API : GET + "/"
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
 
-// API : GET + "http://localhost:1234/test"
-app.get('/test', function(req, res) {
-    res.send('TEST SUCCESS');
-})
+// API : GET + "/test"
+app.get("/test", function (req, res) {
+    res.send("TEST SUCCESS");
+});
 
-// API : GET + "http://localhost:1234/test/1"
-app.get('/test/1', function(req, res) {
-    res.send('One!');
-})
+// API : GET + "/test/1"
+app.get("/test/1", function (req, res) {
+    res.send("One!");
+});
 
-// API : GET + /hello, /bye, /nicetomettyou 
-app.get('/hello', function(req, res) {
-    res.send('Hello!');
-})
+// API : GET + "/hello"
+app.get("/hello", function (req, res) {
+    res.json({
+        say: "Hello!",
+    });
+});
 
-app.get('/bye', function(req, res) {
-    res.send('Bye!');
-})
+// API : GET + "/bye"
+app.get("/bye", function (req, res) {
+    res.json({
+        say: "Bye!",
+    });
+});
 
-app.get('/nicetomeetyou', function(req, res) {
-    res.send('Nice to meet U!');
-})
+// API : GET + "/nicetomeetyou"
+app.get("/nicetomeetyou", function (req, res) {
+    res.json({
+        say: "Nice 2 meet u!",
+    });
+});
+
+// API : GET + "/products/1"
+app.get("/products/1", function (req, res) {
+    res.json({
+        title: "객체를 공부해보자",
+        price: "20000",
+        description: "객체에 대해 완벽히 이해해보세요.",
+    });
+});
