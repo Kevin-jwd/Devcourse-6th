8dc666e29a4185a6bf64e84672fa19759a5c95bb
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-14 05:22:11 +0900

    feat: youtube 동영상 주소 테스트 추가

--- 4주차/node_base/params-demo.js
+++ 4주차/node_base/params-demo.js
@@ -7,10 +7,20 @@ app.get("/products/:n", function (req, res) {
     res.json(req.params);
 });
 
-// Youtube path test
-app.get("/:nickname", function (req, res) {
-    const param = req.params;
+// // Youtube channel path test
+// app.get("/:nickname", function (req, res) {
+//     const param = req.params;
+//     res.json({
+//         channel: param.nickname,
+//     });
+// });
+
+// Youtube video path test
+// https://www.youtube.com/watch?v=pSVAgMIc9-c&t=580s
+app.get("/watch", function (req, res) {
+    const q = req.query;
     res.json({
-        channel: param.nickname,
+        video: q.v,
+        timeline: q.t,
     });
 });
