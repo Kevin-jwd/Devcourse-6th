2bb3123283047caccfee35ec36d0934e1070f4bc
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-14 04:47:50 +0900

    feat: youtube 채널 주소 테스트 추가

--- 4주차/node_base/params-demo.js
+++ 4주차/node_base/params-demo.js
@@ -6,3 +6,11 @@ app.listen(1234);
 app.get("/products/:n", function (req, res) {
     res.json(req.params);
 });
+
+// Youtube path test
+app.get("/:nickname", function (req, res) {
+    const param = req.params;
+    res.json({
+        channel: param.nickname,
+    });
+});
